2b991c5ba68c965f742cc0860827696ce6965d99
Andrea Jemmett <andreajemmett@gmail.com>
2014-09-30 12:40:02 +0200

    Mostly done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+(spectrum = function(){
+	var colors = [
+		'#8F0000',
+		'#009900',
+		'#CCFF66',
+		'#FF6600',
+		'#3399FF',
+		'#66FFCC'];
+	var hue1 = colors[(Math.floor(Math.random() * colors.length))];
 
+	$('.jumbo h1 a').css('border-bottom-color', hue1);
+})();
+window.setInterval(spectrum, 1000);
